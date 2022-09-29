9451980c77a141a6a6ee6b642b73fa184851533f
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-29 11:06:52 -0300

    Add: Using Fetch class

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -80,7 +80,7 @@ class User {
 
     static getUsersStorage() {
 
-        return HttpRequest.get('/users');
+        return Fetch.get('/users');
 
     }
 
@@ -108,11 +108,11 @@ class User {
 
             if (this.id) {
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
 
             } else {
 
-                promise = HttpRequest.post(`/users`, this.toJSON());
+                promise = Fetch.post(`/users`, this.toJSON());
 
             }
 
@@ -135,7 +135,7 @@ class User {
 
     remove(){
 
-        return HttpRequest.delete(`/users/${this.id}`);
+        return Fetch.delete(`/users/${this.id}`);
 
     }
 
